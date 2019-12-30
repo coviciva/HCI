be6f36fb280e490b1b149d6d615b4b072b978b60
mvuco00 <mvuco00@fesb.hr>
2019-12-30 17:35:36 +0100

    tagovi

--- gatsbyproject/src/components/header1.js
+++ gatsbyproject/src/components/header1.js
@@ -1,43 +1,38 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import Navigation from "../components/navigation"
 import "./header1.css"
 import { useStaticQuery, graphql } from "gatsby"
 import LogoLink from "../components/logo"
 
-
 export function initNetlifyIdentity() {
-  console.log('initNetlify called.')
-  const script = document.createElement("script");
+  console.log("initNetlify called.")
+  const script = document.createElement("script")
 
-  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
-  script.async = true;
+  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js"
+  script.async = true
 
-  document.body.appendChild(script);
+  document.body.appendChild(script)
 }
 
 function openNetlifyModal() {
-  const netlifyIdentity = window.netlifyIdentity;
+  const netlifyIdentity = window.netlifyIdentity
 
-  if(netlifyIdentity)
-  netlifyIdentity.open();
-  else
-  console.log('netlifyIdentity not defined')
-
+  if (netlifyIdentity) netlifyIdentity.open()
+  else console.log("netlifyIdentity not defined")
 }
 
 class NetlifyIdentity extends Component {
   componentDidMount() {
-    initNetlifyIdentity();
+    initNetlifyIdentity()
   }
 
   render() {
-    return (<div></div>)
+    return <div></div>
   }
-
 }
 
 const Header1 = props => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     {
       site {
         siteMetadata {
@@ -52,23 +47,27 @@ const Header1 = props => {
 
   //const {MenuItems} = data.site.siteMetadata, doli se u viticaste doda MenuItems
 
-    console.log(props)
-    return (
-      
-      <div className="header">
-        <NetlifyIdentity />
-        <LogoLink/>
-        
-        <div className="header-right">          
-            <Navigation MenuItems = {data.site.siteMetadata.MenuItems}/>
-            <button type="button" className="myButton" onClick = {() => {openNetlifyModal()}}>LOG IN</button>
-            <div className="hamburger">III</div>
-        </div>
+  console.log(props)
+  return (
+    <div className="header">
+      <NetlifyIdentity />
+      <LogoLink />
+
+      <div className="header-right">
+        <Navigation MenuItems={data.site.siteMetadata.MenuItems} />
+        <button
+          type="button"
+          className="myButton"
+          onClick={() => {
+            openNetlifyModal()
+          }}
+        >
+          LOG IN
+        </button>
+        <div className="hamburger">III</div>
       </div>
-      
-      
-        
-    )
+    </div>
+  )
 }
 
 export default Header1
